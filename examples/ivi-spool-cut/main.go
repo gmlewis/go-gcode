69fbbfafca8fa51d3882bd1c37a7cdb29053fbc1
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-09-21 12:53:52 -0400

    Minor tweak

--- examples/ivi-spool-cut/main.go
+++ examples/ivi-spool-cut/main.go
@@ -17,7 +17,7 @@ const (
 
 	cutStep  = 0.5
 	cutZ     = -4.0
-	feedrate = 60
+	feedrate = 80
 	safeZ    = 5.0
 )
 
@@ -38,11 +38,13 @@ func gcmc() *GCode {
 	g.GotoXY(offset.Add(X(0.5 * spindleDiam)))
 	g.MoveZWithF(feedrate, Z(cutZ))
 	g.MoveX(offset.Add(X(0.5 * wireFinalDiam)))
+	g.MoveX(offset.Add(X(0.5 * spindleDiam)))
 	g.MoveZ(Z(safeZ))
 
 	g.GotoXY(offset.Add(X(-0.5 * spindleDiam)))
 	g.MoveZ(Z(cutZ))
 	g.MoveX(offset.Add(X(-0.5 * wireFinalDiam)))
+	g.MoveX(offset.Add(X(-0.5 * spindleDiam)))
 	g.MoveZ(Z(safeZ))
 
 	g.GotoXYZ(home)
